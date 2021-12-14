d636eebb4572adf71ce159d5981875da3d62318e
Joshua Taylor <joshua.taylor@thoughtworks.com>
2021-12-14 11:33:23 +0000

    Host on AWS Elastic Beanstalk
    - Zip up entire application and source
    - Upload to an S3 bucket
    - Create ElasticBeanstalk application, using source bundle

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -24,5 +24,25 @@ resource "aws_s3_bucket" "recipe_manager_deployment" {}
 resource "aws_s3_bucket_object" "recipe_manager_deployment" {
   bucket = aws_s3_bucket.recipe_manager_deployment.bucket
   key    = "recipe_manager_deployment"
+  source = "bundle.zip"
+}
+
+resource "aws_elastic_beanstalk_application" "recipe_manager" {
+  name        = "Recipe Manager"
+  description = "Created as an exercise for NextJS"
+}
+
+resource "aws_elastic_beanstalk_application_version" "recipe_manager_main" {
+  name        = "recipe_manager_main"
+  application = aws_elastic_beanstalk_application.recipe_manager.name
+  bucket      = aws_s3_bucket.recipe_manager_deployment.id
+  key         = aws_s3_bucket_object.recipe_manager_deployment.id
+}
+
+resource "aws_elastic_beanstalk_environment" "recipe_manager" {
+  name                = "dev"
+  application         = aws_elastic_beanstalk_application.recipe_manager.name
+  solution_stack_name = "64bit Amazon Linux 2 v5.4.8 running Node.js 14"
+  version_label       = aws_elastic_beanstalk_application_version.recipe_manager_main.name
 }
 
